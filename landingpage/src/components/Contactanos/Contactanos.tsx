f0008071c0f4fc54e479ea58338a0c0a08097f63
CAMIANAIS <cmamanicr@unsa.edu.pe>
2023-11-27 14:45:47 -0500

    includes basic conten, components

---
+++ landingpage/src/components/Contactanos/Contactanos.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contactanos: React.FC = () => {
+  return (
+    <div>
+      <h1>Contactanos</h1>
+      {/* Información de contacto o formulario de contacto */}
+    </div>
+  );
+}
+
+export default Contactanos;
